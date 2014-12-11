950296a7f122e8ebed270005609f2a1fe96885d6
Oleksis <o.shpylchyn@gmail.com>
2014-12-11 09:53:43 +0100

    Programming Assignment 2
    Completion of cacheMatrix function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a list of functions: to set and get the values of the matrix
+## and the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+        inv <- NULL # setting the value of the inverse matrix to zero, if it exists
+        
+        if(class(x) != "matrix" ) {stop("object is not a matrix")} # checking the class of the input
+        if(det(x) == 0) {stop("the determinant is zero")} # checking whether the determinant of a matrix is zero
+        set <- function(y) { # "set" lets us to assign a new value to "x"
+                x <<- y
+                inv <<- NULL
+        }
 
+        get <- function() {x} # assign the value of the original matrix
+        setinv <- function(solve) {inv <<- solve} # setting and caching ("superassigning") an inverse matrix
+        getinv <- function () {inv} # on subsequent calls getinv will cache the value of an inverse matrix
+        
+        list(get = get, setinv = setinv, getinv = getinv) # list of functions
 }
 
 
-## Write a short comment describing this function
+## cacheSolve calculates the inverse matrix and caches it, thus saving time of accessing
+## the output
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv() # accessing "x" dataframe and getting the inverse matrix
+        
+        if(!is.null(inv)) { # if "inv" is not NULL, then it is in cache
+                message("getting cached data") # print the message on the console
+                return(inv)
+        }
+        
+        data <- x$get() # further code is executed when inv is NULL (not in cache)
+        inv <- solve(data) # calculating inverse matrix
+        x$setinv(inv)
+        inv
 }
